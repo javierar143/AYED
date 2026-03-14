6ca2a4b384e20b242e2fbd331365d34869eb69dc
Javier <javierar143@hotmail.com>
2026-03-14 14:51:18 -0300

    no va a ir este

--- prueba2/Main.java
+++ prueba2/Main.java
@@ -2,7 +2,7 @@ package AYED.prueba2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("El resultado es: ");
+        System.out.println("El resultado es este ");
 
         }
 }
