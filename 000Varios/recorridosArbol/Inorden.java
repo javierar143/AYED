2ab662663ef800e103e2bd4df90c725def4168a9
Javier <javierar143@hotmail.com>
2026-04-02 19:39:48 -0300

    explicacion pasos de construir arbol a partir de recorridos

--- 000Varios/recorridosArbol/Inorden.java
+++ 000Varios/recorridosArbol/Inorden.java
@@ -14,6 +14,92 @@ public void inorden() {
     }
 }
 
+/*
+########El Inorden separa el árbol en izquierda y derecha.######
+Preorden dice la raíz al inicio
+Postorden dice la raíz al final
+
+
+Ejemplo:
+Postorden → el último elemento es la raíz
+Buscás esa raíz en el Inorden
+Eso divide:
+izquierda
+derecha
+
+Paso 1
+
+La raíz es:
+último de postorden
+
+Paso 2
+
+Buscás esa raíz en Inorden:
+izquierda | raíz | derecha
+
+Paso 3
+
+Dividís postorden en:
+izquierda (misma cantidad de nodos que en Inorden izquierda)
+derecha (el resto, antes de la raíz)
+
+Ejemplo concreto:
+Inorden:   [1, 3, 6, 8, 10]
+Postorden: [1, 6, 3, 10, 8]
+
+Paso 1: raíz
+Postorden:
+[1, 6, 3, 10, 8]
+                ↑
+              raíz = 8
+
+Paso 2: dividir Inorden
+[1, 3, 6 | 8 | 10]
+izquierda: [1,3,6]
+derecha: [10]
+
+Paso 3: dividir Postorden
+Sabemos que:
+
+izquierda tiene 3 elementos
+
+Entonces:
+[1, 6, 3 | 10 | 8]
+izquierda: [1,6,3]
+derecha: [10]
+
+Paso 4: subárbol izquierdo
+Inorden:   [1, 3, 6]
+Postorden: [1, 6, 3]
+
+Paso 4: subárbol izquierdo
+Inorden:   [1, 3, 6]
+
+Raíz:
+3
+
+Dividimos:
+[1 | 3 | 6]
+
+Paso 5: subárbol derecho
+Inorden: [10]
+Postorden: [10]
+
+Paso 5: subárbol derecho
+Inorden: [10]
+Postorden: [10]
+
+5. Árbol final
+        8
+       / \
+      3   10
+     / \
+    1   6
+    
+*/
+
+
+
 /*    Inorden:   H /B/ I E -A- F C G   Preorden:  A B H E I C F G
 Raiz: A
 Subizq B H E I
