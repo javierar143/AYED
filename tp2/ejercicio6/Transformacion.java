ae582d60eb41c2347d38950825e38de2ce57233d
Javier <javierar143@hotmail.com>
2026-04-04 12:50:30 -0300

    tp2 ejercicio 6

--- tp2/ejercicio6/Transformacion.java
+++ tp2/ejercicio6/Transformacion.java
@@ -1,5 +1,49 @@
 package tp2.ejercicio6;
+import tp2.ejercicio2.*;
 
 public class Transformacion {
-    
+    BinaryTree<Integer> arbol;
+
+    public Transformacion(BinaryTree<Integer> arbol) {
+        this.arbol = arbol;
+    }
+
+    public BinaryTree<Integer> suma(){
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        if (this.arbol !=null)
+            transformarArbol(this.arbol,tree);
+        
+        return tree;
+    }
+
+    private int transformarArbol(BinaryTree<Integer> original, BinaryTree<Integer> nuevo) {
+        
+        int valor=0;
+        if (original.isLeaf()){
+            valor=original.getData();
+            nuevo.setData(0);
+        }
+
+        else{
+            int izquierdo=0;
+            int derecho=0;
+            
+            if (original.hasLeftChild()){
+                BinaryTree<Integer> tree = new BinaryTree<>();
+                izquierdo = transformarArbol(original.getLeftChild(),tree); 
+                nuevo.addLeftChild(tree);               
+            }
+            
+            if (original.hasRightChild()){
+                BinaryTree<Integer> tree = new BinaryTree<>();
+                derecho  =transformarArbol(original.getRightChild(),tree);
+                nuevo.addRightChild(tree);                 
+            }           
+            nuevo.setData(izquierdo + derecho);
+            valor =original.getData()+ izquierdo + derecho;
+            
+        }    
+
+        return valor;
+    }
 }
--- tp2/ejercicio6/mainTransformacion.java
+++ tp2/ejercicio6/mainTransformacion.java
@@ -1,5 +1,48 @@
 package tp2.ejercicio6;
+import tp2.ejercicio2.*;
 
 public class mainTransformacion {
-    
+    public static void main(String[] args) {
+        BinaryTree<Integer> arbol = new BinaryTree<Integer>(1);
+
+        BinaryTree<Integer> subIzq1 = new BinaryTree<Integer>(2);
+        BinaryTree<Integer> subDer1 = new BinaryTree<Integer>(3);
+
+        //hijos de arbol principal
+        arbol.addLeftChild(subIzq1);
+        arbol.addRightChild(subDer1);
+
+        //armado de rama izq del arbol
+        
+        BinaryTree<Integer> subDer2 = new BinaryTree<Integer>(4);
+        
+        subIzq1.addRightChild(subDer2);
+
+        //armado de rama der del arbol
+        BinaryTree<Integer> subIzq3 = new BinaryTree<Integer>(5);
+        BinaryTree<Integer> subDer3 = new BinaryTree<Integer>(6);
+
+        subDer1.addLeftChild(subIzq3);
+        subDer1.addRightChild(subDer3);
+
+        subIzq3.addLeftChild(new BinaryTree<Integer>(7));
+        subIzq3.addRightChild(new BinaryTree<Integer>(8));
+
+        //impresion preorden
+        arbol.imprimir();
+        System.out.println();
+
+        Transformacion t = new Transformacion(arbol);
+
+        BinaryTree<Integer> arbolNuevo= t.suma();
+
+        System.out.println("Arbol nuevo: ");
+        arbolNuevo.imprimir();
+        System.out.println();
+
+        System.out.println("Arbol original: ");
+        arbol.imprimir();
+        System.out.println();
+
+    }
 }
