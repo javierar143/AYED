c4cf46272d09dfce35bf80d344333ad9211a7b70
Javier <javierar143@hotmail.com>
2026-04-19 21:07:43 -0300

    tp3 ejercicio 6

---
+++ tp3/ejercicio6/RedAguaPotable.java
@@ -0,0 +1,40 @@
+package tp3.ejercicio6;
+import java.util.List;
+
+import tp3.ejercicio1.*;
+
+public class RedAguaPotable {
+    
+    private GeneralTree<Character> arbol;
+
+    public RedAguaPotable(GeneralTree<Character> arbol) {
+        this.arbol = arbol;
+    }
+
+    public double minimoCaudal(double caudal){
+        double cantCaudal = 0;
+        if (this.arbol != null && !this.arbol.isEmpty())
+            cantCaudal = calcularMinimoCaudal(this.arbol,caudal );
+
+        return cantCaudal;
+
+    }
+
+    private double calcularMinimoCaudal(GeneralTree<Character> tree, double caudal) {
+        double caudalCasa=0; 
+        double minimoCaudal = Integer.MAX_VALUE;      
+        if (tree.isLeaf()){ 
+            minimoCaudal=caudal;
+        }
+
+        else {
+            List <GeneralTree<Character>> hijos = tree.getChildren();
+            for (GeneralTree<Character> hijo : hijos){
+                caudalCasa = calcularMinimoCaudal (hijo, caudal/hijos.size());
+                if (caudalCasa < minimoCaudal)
+                    minimoCaudal=caudalCasa;
+            }
+        }
+        return minimoCaudal;
+    }
+}
