449a7b6d7fcaafb70a287ffdfb8a16d6eb6b7643
Javier <javierar143@hotmail.com>
2026-05-28 19:11:41 -0300

    ejercicio 5 finalizado

--- tp5/ejercicio5/SelectorClientesJubilados.java
+++ tp5/ejercicio5/SelectorClientesJubilados.java
@@ -9,38 +9,54 @@ public class SelectorClientesJubilados {
     
     public List <Persona> seleccionarJubilados (Graph <Persona> personas, Persona empleado, int distancia){
         List<Persona> listaJubilados= new ArrayList<>();
-        List<Persona> listaTemp = new ArrayList<>();
-
+        
         if (!personas.isEmpty()){
             Vertex <Persona> empleadoInicial =  personas.search(empleado);
             if (empleadoInicial != null){
                 boolean [] marca = new boolean[personas.getSize()];
                 int pos = empleadoInicial.getPosition();
-                marca[pos] = true;
-                bfsCargarJubilados (pos, personas, distancia, marca, listaJubilados, listaTemp);
+                
+                bfsCargarJubilados (pos, personas, distancia, marca, listaJubilados);
             }
 
         }
-
-        
-
-
         return listaJubilados;
     }
 
     private void bfsCargarJubilados(int pos, Graph<Persona> personas, int distancia, boolean[] marca,
-            List<Persona> listaJubilados, List<Persona> listaTemp) {
+            List<Persona> listaJubilados) {
         
         Queue <Vertex<Persona>> cola = new Queue<Vertex<Persona>>();
         cola.enqueue(personas.getVertex(pos));
         cola.enqueue(null);
+        marca[pos]=true;
         int nivel =0;
         
-        while (!cola.isEmpty()){
-            Vertex<Persona> personaAcual = cola.dequeue();
+        while (!cola.isEmpty() && nivel<=distancia && listaJubilados.size() < 40 ){
+            Vertex<Persona> verticePersona = cola.dequeue();
 
-            if (personaAcual != null && nivel<distancia){
-                List <Edge<Persona>> aristas = personas.getEdges(personaAcual);
+            if (verticePersona != null ){
+                Persona personaAcual = verticePersona.getData();
+                if (personaAcual.EsJubilado() && !personaAcual.isCobroRecibido() ){
+                    listaJubilados.add(personaAcual);
+                }
+                List <Edge<Persona>> aristas = personas.getEdges(verticePersona);
+                for (Edge <Persona> arista : aristas){
+                    Vertex<Persona> vertice = arista.getTarget();
+                    int j = vertice.getPosition();
+                    if (!marca[j]){
+                        marca[j]=true;
+                        cola.enqueue(vertice);
+                    }                    
+                }
+
+            }
+            else {
+               
+                if (!cola.isEmpty()){
+                    nivel++;
+                    cola.enqueue(null);
+                }
             }
         }
         
